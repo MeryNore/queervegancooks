5a6e1721ac9114d43a9a407810a900ac7b987c85
MeryAlice92 <maria92.aguirre@gmail.com>
2025-01-15 22:25:26 +0100

    Ordenar el formulario y añadir validación comensales

--- js/form.js
+++ js/form.js
@@ -22,30 +22,36 @@ function validar () {
     if(!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{2,15}$/.test(nombre)){
         valido = false;
         mensaje = mensaje + 'El nombre sólo puede contener letras y máximo 15 caracteres\n';
-    }
+    };
 
     const apellido = document.getElementById('apellido').value;
     if(!/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{2,40}$/.test(apellido)){
         valido = false;
         mensaje = mensaje + 'El apellido sólo puede contener letras y máximo 40 caracteres\n';
-    }
+    };
 
     const telefono = document.getElementById('telefono').value;
     if (!/^(6|7|8|9){1}[0-9]{8}$/.test(telefono)){
         valido = false;
         mensaje = mensaje + 'El telefono sólo puede contener números y máximo 9 digitos\n';
-    }
+    };
     
     const email = document.getElementById('email').value;
     if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/.test(email)){
         valido = false;
         mensaje = mensaje + 'El campo de email debe ser en formato algo@algo.algo\n';
-    }
+    };
 
-    const chekcboxk = document.getElementById('acepto')
+    const chekcboxk = document.getElementById('acepto');
     if (!chekcboxk.checked){
         valido = false;
-        mensaje = mensaje + "Debe aceptar la POLÍTICA DE PRIVACIDAD"
+        mensaje = mensaje + "Debe aceptar la POLÍTICA DE PRIVACIDAD\n"
+    };
+
+    const comensales = document.getElementById('comensales').value;
+    if(comensales <= parseInt(1)){
+        valido = false;
+        mensaje = mensaje + "La candidad mínima de comensales son dos persona"
     }
 
     //si todo está ok, enviará el formulario
